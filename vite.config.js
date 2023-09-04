f43580d62a6ef1f5b0a28f9c1dbfcc08e4d763e3
Gabbyroba <gabbyramosbr2@gmail.com>
2023-09-04 14:55:19 -0300

    Projeto Finalizado

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/picky-kitty/",
+  base: "/picky-kitty-store/",
   plugins: [],
   resolve: {
     /*something*/
